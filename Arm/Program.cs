aab8349e190c1df9fde578185cefd3bdc019660c
Jyunaut <edmartinh@gmail.com>
2020-07-16 12:42:22 -0700

    added project files

---
+++ Arm/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace Arm
+{
+    class Program
+    {
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Window());
+        }
+    }
+}
---
+++ Arm/Window.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace Arm
+{
+    public partial class Window : Form
+    {
+        public Stopwatch stopwatch { get; set; }
+
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Load(object sender, EventArgs e)
+        {
+            stopwatch = Stopwatch.StartNew();
+            port.Open();
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (stopwatch.ElapsedMilliseconds > 15)
+            {
+                stopwatch = Stopwatch.StartNew();
+                port.Write(String.Format("{0}", e.X));
+                Console.WriteLine(e.X);
+            }
+        }
+    }
+}
